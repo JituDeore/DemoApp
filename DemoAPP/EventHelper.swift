0c376d7f76bbe8237308ec4ddd92621921a7337e
Jitendra Deore <jitendrad.winjit@verse.in>
2016-12-20 15:13:59 +0530

    Added EventHelper Class to genrate the appropriate message.

---
+++ DemoAPP/EventHelper.swift
@@ -0,0 +1,12 @@
+//
+//  EventHelper.swift
+//  DemoAPP
+//
+//  Created by Jitendra-Deore on 20/12/16.
+//  Copyright © 2016 Jitendra-Deore. All rights reserved.
+//
+
+import UIKit
+
+class EventHelper: NSObject {
+}
