0d54d933b05006a9438bb1a71837b0d39dc9f00a
Jitendra Deore <jitendrad.winjit@verse.in>
2016-12-20 16:01:36 +0530

    Added content filter class to filter the content.

---
+++ DemoAPP/ContentFilter.swift
@@ -0,0 +1,13 @@
+//
+//  ContentFilter.swift
+//  DemoAPP
+//
+//  Created by Jitendra-Deore on 20/12/16.
+//  Copyright © 2016 Jitendra-Deore. All rights reserved.
+//
+
+import UIKit
+
+class ContentFilter: NSObject {
+
+}
