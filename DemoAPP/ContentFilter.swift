b267c8134c2d4a083be3e9b5c6ed9766f8279743
Jitendra Deore <jitendrad.winjit@verse.in>
2016-12-21 12:54:46 +0530

    Added logic to get the link and replace it with the *** Added network checker class.

--- DemoAPP/ContentFilter.swift
+++ DemoAPP/ContentFilter.swift
@@ -8,6 +8,97 @@
 
 import UIKit
 
-class ContentFilter: NSObject {
+@objc class ContentFilter: NSObject {
+    
+    var finalString: String?
+    var titleArray: [String] = []
+    var linkArray: [String] = []
+    
+    var link: String?
+    
+    func filterTheMessage(str: String) {
+        
+        finalString = self.skipPhoneNumberFromMessage(str)
+        finalString = self.replaceEmailFromMesage(finalString!)
+        
+        print(str)
+    }
+    
+    func skipPhoneNumberFromMessage(str: String)-> String{
 
+        if (str.rangeOfCharacterFromSet(NSCharacterSet.decimalDigitCharacterSet()) != nil){
+            
+            var strPhoneNumber: String?
+            strPhoneNumber = (str.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet()) as NSArray).componentsJoinedByString("****")
+            
+            strPhoneNumber = strPhoneNumber?.stringByReplacingOccurrencesOfString("+", withString: "")
+            
+            return strPhoneNumber ?? ""
+        }
+        
+        return str
+    }
+    
+    
+    func replaceEmailFromMesage(str:String)-> String{
+        
+        var strEmail: String?
+        
+        if str.containsString("https://www.carlist.my"){
+            
+            // Then and then get the title from URL..
+            
+            let  detect = try! NSDataDetector(types: NSTextCheckingType.Link.rawValue)
+            let matches = detect.matchesInString(str, options: [], range: NSMakeRange(0, str.characters.count))
+            
+            for match in matches {
+                let url = (str as NSString).substringWithRange(match.range)
+                
+                let titleStr = self.getTitleFromURL(NSURL.init(string: url))
+                
+                titleArray.append(titleStr)
+                
+                return strEmail ?? ""
+            }
+            
+            link = strEmail ?? ""
+
+            print(titleArray)
+        
+        }else{
+            
+            let  detect = try! NSDataDetector(types: NSTextCheckingType.Link.rawValue)
+            let matches = detect.matchesInString(str, options: [], range: NSMakeRange(0, str.characters.count))
+            
+            for match in matches {
+                let url = (str as NSString).substringWithRange(match.range)
+                strEmail = str
+                    .stringByReplacingOccurrencesOfString(url, withString: "******")
+            
+                return strEmail ?? ""
+            }
+        }
+        return ""
+    }
+    
+    func getTitleFromURL(url: NSURL?)->String{
+        
+        let htmlCode = try! String(contentsOfURL: url!, encoding: NSASCIIStringEncoding)
+        let start = "<title>"
+        let range1 = (htmlCode as NSString).rangeOfString(start)
+        let end = "</title>"
+        let range2 = (htmlCode as NSString).rangeOfString(end)
+        
+        var str: NSString
+        str = htmlCode
+    
+        let finalString =  str.substringWithRange(NSMakeRange(range1.location + 7, range2.location - range1.location - 7))
+        
+        print("substring is \(finalString)")
+        //I Used +7 and -7 in NSMakeRange to eliminate the length of <title> i.e 7
+        
+        return finalString
+        
+    }
+    
 }
--- DemoAPP/ViewController.swift
+++ DemoAPP/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var messageTextView: UITextView!
+    let contentFilter = ContentFilter()
+    var titleMessage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func sendButtonTapped(sender: AnyObject) {
+        
+        
+        if messageTextView.text == nil || messageTextView.text.isEmpty{
+            let alert = UIAlertController(title: nil, message: "Please enter the message", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title:"Ok", style: .Default, handler: {(action: UIAlertAction) -> Void in
+            })
+            alert.addAction(okAction)
+            self.presentViewController(alert, animated: true, completion: { _ in })
+            
+        }else{
+            contentFilter.filterTheMessage(messageTextView.text)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
